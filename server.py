8c67790cbc1d067eeb25b93e002949533d8407af
Jim Yeh <lemonlatte@gmail.com>
2014-10-19 01:05:01 +0800

    Exercise 1

--- server.py
+++ server.py
@@ -15,5 +15,24 @@ def hi():
     name = request.args.get("name", "Jim")
     return "Hi, %s" % name
 
+
+@app.route("/my_dict")
+def my_dict():
+
+    dictionary = {
+        "apple": "a fruit with red color.",
+        "banana": "a fruit with yellow color.",
+        "kiwi": "a fruit with brown color.",
+        "melon": "a fruit with green color.",
+        "watermelon": "a popular fruit in summer."
+    }
+
+    q = request.args.get("q", None)
+
+    if q in dictionary:
+        return dictionary[q]
+    else:
+        return "Not found"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
